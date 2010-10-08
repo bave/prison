5f5c4cb63e113fc6c87f977b036e059604f8f41f
bave <inoue.tomoya@gmail.com>
2010-10-08 18:41:34 +0900

    update

--- eval/eval.sh
+++ eval/eval.sh
@@ -16,13 +16,14 @@ fi
 
 i=1
 START_PORT=$SPORT
+END_PORT=`expr $START_PORT + $PER_NODE - 1`
 while [ $i -le $DIV ]
 do
     echo "./bootstrap.sh $NODE $START_PORT  $END_PORT $INTERNAL_SOCKET$i"
-    ./bootstrap.sh $NODE $START_PORT  $END_PORT $INTERNAL_SOCKET$i"
+    ./bootstrap.sh $NODE $START_PORT  $END_PORT $INTERNAL_SOCKET$i
     echo "./put.sh $NODE $START_PORT $END_PORT $INTERNAL_SOCKET$i"
-    ./put.sh $NODE $START_PORT $END_PORT $INTERNAL_SOCKET$i"
-    END_PORT=`expr $START_PORT + $PER_NODE - 1`
+    ./put.sh $NODE $START_PORT $END_PORT $INTERNAL_SOCKET$i
     START_PORT=`expr $END_PORT + 1`
+    END_PORT=`expr $START_PORT + $PER_NODE - 1`
     i=`expr $i + 1`
 done

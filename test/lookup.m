a47c8678c8829be69c0c2fca761c1b7044704164
bave <inoue.tomoya@gmail.com>
2011-05-18 05:40:46 +0900

    update

---
+++ test/lookup.m
@@ -0,0 +1,31 @@
+#import <Cocoa/Cocoa.h>
+
+#include "../ps.h"
+
+int main(int argc, char** argv)
+{
+    id pool = [NSAutoreleasePool new];
+
+    id ps = [[PrisonSock new] autorelease];
+    id peer_id = nil;
+
+    // configuration
+    [ps set_node_name:@"prison"];
+    [ps set_sock_path:@"/tmp/sock_cage"];
+
+    // create prison socket
+    BOOL retval = [ps ps_create];
+    if (retval) {
+        if (argc != 2) { 
+            peer_id = [ps ps_lookup:@"aris@ps"];
+        } else {
+            id tmp = [NSString stringWithUTF8String:argv[1]];
+            peer_id = [ps ps_lookup:tmp];
+        }
+        NSLog(@"%@", peer_id);
+    }
+    [ps ps_destroy];
+
+    [pool drain];
+    return 0;
+}
